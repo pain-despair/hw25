48ce3f15e8279e499f080c0c906660b6eb787891
pain-despair <slavikrymar08@gmail.com>
2023-11-24 14:12:56 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const table = document.getElementById('numberTable');
+
+//     for (let i = 1; i <= 10; i++) {
+//         const row = document.createElement('tr');
+
+//         for (let j = 1; j <= 10; j++) {
+//             const cell = document.createElement('td');
+//             const number = (i - 1) * 10 + j;
+//             cell.textContent = number;
+//             row.appendChild(cell);
+//         }
+
+//         table.appendChild(row);
+//     }
+// });
